26fe88b8a558a9648bc091322d853ccb6d4c6274
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-02-09 12:06:27 -0800

    add to summary page

--- src/routes/SummaryPage.js
+++ src/routes/SummaryPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 import PetContext from "../context/PetContext";
 
 class SummaryPage extends Component {
@@ -9,6 +9,7 @@ class SummaryPage extends Component {
     const { catAdopter, cat } = this.context;
     return (
       <>
+        <img src={cat.imageUrl} />
         <h3>Congratulations {catAdopter}!</h3>
         <p>
           You have just adopted {cat.name}, a {cat.age} year old {cat.sex}{" "}
@@ -17,6 +18,9 @@ class SummaryPage extends Component {
         <p>
           We will contact you in the next 24 hours for further adoption details.
         </p>
+        <Link to="/">
+          <button>Go back to home page</button>
+        </Link>
       </>
     );
   }
